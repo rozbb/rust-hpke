10abb20619b3377d6321034de6405d818c617be6
Michael Rosenberg <michael@mrosenberg.pub>
2023-07-17 01:49:57 -0400

    More comments and cleanup to xyber; pinned serde-big-array

--- src/kem/xyber768d00.rs
+++ src/kem/xyber768d00.rs
@@ -11,6 +11,7 @@ use generic_array::{
     typenum::{self, Unsigned},
     GenericArray,
 };
+use pqc_kyber::Keypair as KyberKeypair;
 use rand_core::{CryptoRng, RngCore};
 use subtle::{Choice, ConstantTimeEq};
 
@@ -30,7 +31,7 @@ impl Serializable for EncappedKey {
     type OutputSize = U1120;
 
     fn to_bytes(&self) -> GenericArray<u8, Self::OutputSize> {
-        // Output DH encapped key || Kyber encapped key. That's 32 bytes + 1088 bytes += 1120.
+        // Output X25519 encapped key || Kyber encapped key. That's 32 bytes + 1088 bytes += 1120.
         self.x
             .to_bytes()
             .concat(*<GenericArray<u8, U1088>>::from_slice(&self.k[..]))
@@ -41,7 +42,7 @@ impl Deserializable for EncappedKey {
     fn from_bytes(encoded: &[u8]) -> Result<Self, HpkeError> {
         enforce_equal_len(Self::OutputSize::to_usize(), encoded.len())?;
 
-        // Grab the DH encapped key then the Kyber encapped key. The unwrap() is permitted because
+        // Grab the X25519 encapped key then the Kyber encapped key. The unwrap() is permitted because
         // of the enforce_equal_len above.
         let x = <<X25519HkdfSha256 as KemTrait>::EncappedKey as Deserializable>::from_bytes(
             &encoded[..32],
@@ -57,7 +58,7 @@ impl Serializable for PublicKey {
     type OutputSize = U1216;
 
     fn to_bytes(&self) -> GenericArray<u8, Self::OutputSize> {
-        // Output DH pubkey || Kyber pubkey. That's 32 bytes + 1184 bytes += 1216.
+        // Output X25519 pubkey || Kyber pubkey. That's 32 bytes + 1184 bytes += 1216.
         self.x
             .to_bytes()
             .concat(*<GenericArray<u8, U1184>>::from_slice(&self.k[..]))
@@ -68,7 +69,7 @@ impl Deserializable for PublicKey {
     fn from_bytes(encoded: &[u8]) -> Result<Self, HpkeError> {
         enforce_equal_len(Self::OutputSize::to_usize(), encoded.len())?;
 
-        // Grab the DH pubkey then the Kyber pubkey. The unwrap() is permitted because of the
+        // Grab the X25519 pubkey then the Kyber pubkey. The unwrap() is permitted because of the
         // enforce_equal_len above.
         let x = <<X25519HkdfSha256 as KemTrait>::PublicKey as Deserializable>::from_bytes(
             &encoded[..32],
@@ -84,7 +85,7 @@ impl Serializable for PrivateKey {
     type OutputSize = U2432;
 
     fn to_bytes(&self) -> GenericArray<u8, Self::OutputSize> {
-        // Output DH privkey || Kyber privkey. That's 32 bytes + 1184 bytes += 1216.
+        // Output X25519 privkey || Kyber privkey. That's 32 bytes + 1184 bytes += 1216.
         self.x
             .to_bytes()
             .concat(*<GenericArray<u8, U2400>>::from_slice(&self.k[..]))
@@ -95,7 +96,7 @@ impl Deserializable for PrivateKey {
     fn from_bytes(encoded: &[u8]) -> Result<Self, HpkeError> {
         enforce_equal_len(Self::OutputSize::to_usize(), encoded.len())?;
 
-        // Grab the DH privkey then the Kyber privkey. The unwrap() is permitted because of the
+        // Grab the X25519 privkey then the Kyber privkey. The unwrap() is permitted because of the
         // enforce_equal_len above.
         let x = <<X25519HkdfSha256 as KemTrait>::PrivateKey as Deserializable>::from_bytes(
             &encoded[..32],
@@ -106,24 +107,31 @@ impl Deserializable for PrivateKey {
     }
 }
 
+/// An X25519-Kyber768 public key. This holds an X25519 public key and a Kyber768 public key.
 #[derive(Debug, PartialEq, Eq, Clone)]
 #[cfg_attr(feature = "serde_impls", derive(serde::Serialize, serde::Deserialize))]
+#[doc(hidden)]
 pub struct PublicKey {
     x: <X25519HkdfSha256 as KemTrait>::PublicKey,
     #[cfg_attr(feature = "serde_impls", serde(with = "serde_big_array::BigArray"))]
     k: pqc_kyber::PublicKey,
 }
 
+/// An X25519-Kyber768 private key. This holds an X25519 private key and a Kyber768 private key.
 #[derive(Clone)]
 #[cfg_attr(feature = "serde_impls", derive(serde::Serialize, serde::Deserialize))]
+#[doc(hidden)]
 pub struct PrivateKey {
     x: <X25519HkdfSha256 as KemTrait>::PrivateKey,
     #[cfg_attr(feature = "serde_impls", serde(with = "serde_big_array::BigArray"))]
     k: pqc_kyber::SecretKey,
 }
 
+/// Holds the content of an encapsulated secret. This is what the receiver uses to derive the
+/// shared secret. Since this is a hybrid KEM, it holds a DH encapped key and a Kyber encapped key.
 #[derive(Clone)]
 #[cfg_attr(feature = "serde_impls", derive(serde::Serialize, serde::Deserialize))]
+#[doc(hidden)]
 pub struct EncappedKey {
     x: <X25519HkdfSha256 as KemTrait>::EncappedKey,
     #[cfg_attr(feature = "serde_impls", serde(with = "serde_big_array::BigArray"))]
@@ -148,7 +156,7 @@ impl Eq for PrivateKey {}
 pub struct X25519Kyber768Draft00;
 
 impl KemTrait for X25519Kyber768Draft00 {
-    // X25519Kyber768Draft00 §5: Nsecret of X25519Kyber768Draft00 is 32
+    // X25519Kyber768Draft00 §5: Nsecret of X25519Kyber768Draft00 is 64
     #[doc(hidden)]
     type NSecret = typenum::U64;
 
@@ -160,39 +168,40 @@ impl KemTrait for X25519Kyber768Draft00 {
 
     // X25519Kyber768Draft00 §3.3
     // def DeriveKeyPair(ikm):
-    //     dkp_prk = LabeledExtract("", "dkp_prk", ikm)
-    //     seed = LabeledExpand(dkp_prk, "sk", 32 + 64)
-    //     seed1 = seed[0:32]
-    //     seed2 = seed[32:96]
-    //     sk1, pk1 = DHKEM.DeriveKeyPair(seed1)
-    //     sk2, pk2 = Kyber768Draft00.DeriveKeyPair(seed2)
-    //     return (concat(sk1, sk2), concat(pk1, pk2))
+    //   dkp_prk = LabeledExtract("", "dkp_prk", ikm)
+    //   seed = LabeledExpand(dkp_prk, "sk", 32 + 64)
+    //   seed1 = seed[0:32]
+    //   seed2 = seed[32:96]
+    //   sk1, pk1 = X25519KEM.DeriveKeyPair(seed1)
+    //   sk2, pk2 = Kyber768Draft00.DeriveKeyPair(seed2)
+    //   return (concat(sk1, sk2), concat(pk1, pk2))
 
     /// Deterministically derives a keypair from the given input keying material and ciphersuite
-    /// ID. The keying material SHOULD have as many bits of entropy as the bit length of a secret
-    /// key, i.e., 19,456.
+    /// ID. The keying material should have as many bits of entropy as it extracts for its seeds,
+    /// i.e., 768 bits.
     fn derive_keypair(ikm: &[u8]) -> (Self::PrivateKey, Self::PublicKey) {
+        // Hash the IKM
         let suite_id = kem_suite_id::<Self>();
         let (_, dkp_prk) = labeled_extract::<HkdfSha256>(&[], &suite_id, b"dkp_prk", ikm);
+
+        // Expand the randomness to fill 2 seeds
         let mut buf = [0u8; 32 + 64];
         dkp_prk
             .labeled_expand(&suite_id, b"sk", &[], &mut buf)
             .unwrap();
         let (seed1, seed2) = buf.split_at(32);
+
+        // Generate the keypairs with the two seeds
         let (skx, pkx) = X25519HkdfSha256::derive_keypair(seed1);
-        let kpk = pqc_kyber::derive(seed2).unwrap();
-        (
-            PrivateKey {
-                x: skx,
-                k: kpk.secret,
-            },
-            PublicKey {
-                x: pkx,
-                k: kpk.public,
-            },
-        )
+        let KyberKeypair {
+            public: pkk,
+            secret: skk,
+        } = pqc_kyber::derive(seed2).unwrap();
+
+        (PrivateKey { x: skx, k: skk }, PublicKey { x: pkx, k: pkk })
     }
 
+    /// Converts a X25519-Kyber768 private key to a public key
     fn sk_to_pk(sk: &PrivateKey) -> PublicKey {
         PublicKey {
             x: X25519HkdfSha256::sk_to_pk(&sk.x),
@@ -200,47 +209,73 @@ impl KemTrait for X25519Kyber768Draft00 {
         }
     }
 
+    // X25519Kyber768Draft00 §3.4
+    //
+    // def Encap(pkR):
+    //   (pkA, pkB) = pkR
+    //   (ss1, enc1) = X25519KEM.Encap(pkA)
+    //   (ss2, enc2) = Kyber768Draft00.Encap(pkB)
+    //   return (
+    //     concat(ss1, ss2),
+    //     concat(enc1, enc2)
+    //   )
+
+    /// Does an X25519-Kyber768 encapsulation. This does not support sender authentication.
+    /// `sender_id_keypair` must be `None`. Otherwise, this returns
+    /// [`HpkeError::AuthnotSupportedError`].
     fn encap<R: CryptoRng + RngCore>(
         pk_recip: &Self::PublicKey,
         sender_id_keypair: Option<(&Self::PrivateKey, &Self::PublicKey)>,
         csprng: &mut R,
     ) -> Result<(SharedSecret<Self>, Self::EncappedKey), HpkeError> {
+        // Kyber doesn't support sender authentication
         if sender_id_keypair.is_some() {
             return Err(HpkeError::AuthNotSupportedError);
         }
 
+        // Encap using both KEMs
         let (ss1, enc1) = X25519HkdfSha256::encap(&pk_recip.x, None, csprng)?;
-        let (enc2, ss2) = match pqc_kyber::encapsulate(&pk_recip.k, csprng) {
-            Ok(res) => res,
-            Err(_e) => return Err(HpkeError::EncapError),
-        };
+        let (enc2, ss2) =
+            pqc_kyber::encapsulate(&pk_recip.k, csprng).map_err(|_| HpkeError::EncapError)?;
 
+        // Compute X25519 shared secret || Kyber shared secret. The unwrap() is OK because ss1.0
+        // and ss2 are fixed-size arrays.
         let mut ss = <SharedSecret<Self> as Default>::default();
-        ss.0 = ss1
-            .0
-            .concat(*<GenericArray<u8, typenum::U32>>::from_slice(&ss2[..]));
+        ss.0 = ss1.0.concat(ss2.try_into().unwrap());
         Ok((ss, EncappedKey { x: enc1, k: enc2 }))
     }
 
+    // X25519Kyber768Draft00 §3.4
+    // def Decap(enc, skR):
+    //   (skA, skB) = skR
+    //   enc1 = enc[0:32]
+    //   enc2 = enc[32:1120]
+    //   ss1 = DHKEM.Decap(enc1, skA)
+    //   ss2 = Kyber768Draft00.Decap(enc2, skB)
+    //   return concat(ss1, ss2)
+
+    /// Does an X25519-Kyber768 decapsulation. This does not support sender authentication.
+    /// `pk_sender_id` must be `None`. Otherwise, this returns
+    /// [`HpkeError::AuthnotSupportedError`].
     fn decap(
         sk_recip: &Self::PrivateKey,
         pk_sender_id: Option<&Self::PublicKey>,
         encapped_key: &Self::EncappedKey,
     ) -> Result<SharedSecret<Self>, HpkeError> {
+        // Kyber doesn't support sender authentication
         if pk_sender_id.is_some() {
             return Err(HpkeError::AuthNotSupportedError);
         }
 
+        // Decapsulate with both KEMs
         let ss1 = X25519HkdfSha256::decap(&sk_recip.x, None, &encapped_key.x)?;
-        let ss2 = match pqc_kyber::decapsulate(&encapped_key.k, &sk_recip.k) {
-            Ok(ss) => ss,
-            Err(_e) => return Err(HpkeError::DecapError),
-        };
+        let ss2 = pqc_kyber::decapsulate(&encapped_key.k, &sk_recip.k)
+            .map_err(|_| HpkeError::DecapError)?;
 
+        // Compute X25519 shared secret || Kyber shared secret. The unwrap() is OK because ss1.0
+        // and ss2 are fixed-size arrays.
         let mut ss = <SharedSecret<Self> as Default>::default();
-        ss.0 = ss1
-            .0
-            .concat(*<GenericArray<u8, typenum::U32>>::from_slice(&ss2[..]));
+        ss.0 = ss1.0.concat(ss2.try_into().unwrap());
         Ok(ss)
     }
 }
